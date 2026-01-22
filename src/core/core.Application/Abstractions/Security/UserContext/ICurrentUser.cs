a06f4c09d218835848bf24bb41c2241519c92b30
eren karabulut <erenkrabulut@gmail.com>
2026-01-22 16:18:14 +0300

    Add Infrastructure DI and CurrentUser implementation

--- src/core/core.Application/Abstractions/Security/UserContext/ICurrentUser.cs
+++ src/core/core.Application/Abstractions/Security/UserContext/ICurrentUser.cs
@@ -8,8 +8,9 @@ namespace core.Application.Abstractions.Security.UserContext
 {
     public interface ICurrentUser
     {
-        Guid UserId { get; }
+        Guid? UserId { get; }
         IReadOnlyCollection<string> Roles { get; }
         IReadOnlyCollection<string> Permissions { get; }
+        bool IsAuthenticated { get; }
     }
 }
---
+++ src/core/core.Infrastructure/Security/UserContext/CurrentUser.cs
@@ -0,0 +1,45 @@
+﻿using core.Application.Abstractions.Security.UserContext;
+using core.Infrastructure.Security.Tokens;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core.Infrastructure.Security.UserContext
+{
+    public class CurrentUser: ICurrentUser
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUser(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private ClaimsPrincipal? User => _httpContextAccessor.HttpContext?.User;
+
+        public Guid? UserId
+        {
+            get
+            {
+                var claim = User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (Guid.TryParse(claim, out var id))
+                    return id;
+                return null;
+            }
+        }
+
+        public IReadOnlyCollection<string> Roles =>
+            User?.FindAll(ClaimTypes.Role).Select(c => c.Value).ToList() ?? new List<string>();
+
+        public IReadOnlyCollection<string> Permissions =>
+            User?.FindAll(CustomClaimTypes.Permission).Select(c => c.Value).ToList() ?? new List<string>();
+
+        public bool IsAuthenticated => User?.Identity?.IsAuthenticated ?? false;
+
+
+    }
+}
