38128a4264ad96bab984b8c0328574874750220c
eren karabulut <erenkrabulut@gmail.com>
2026-01-31 17:40:24 +0300

    feat(webapi): add anonymous /health endpoint for uptime checks

---
+++ src/hosts/WebAPI/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public sealed class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "ok" });
+        }
+
+    }
+}
